f36842ff84ca2f649c34356ed60a91f143410297
RaissaGois <168118466+RaissaGois@users.noreply.github.com>
2025-02-18 17:20:55 -0300

    Add files via upload

---
+++ Lampada.vs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lampada
+{
+    class Lampada
+    {
+        public int lampadaNro;
+        public string comodo;
+        public bool status;
+
+        public void provarExistencia()
+        {
+            Console.WriteLine("Oi Lâmpada");
+        }
+        static void Main(string[] args)
+
+        {
+            // Console.WriteLine("Olá Mundo!");
+            Lampada l1 = new Lampada();
+
+            l1.provarExistencia();
+
+            l1.lampadaNro = 1;
+            l1.comodo = "quarto";
+            l1.status = true;
+
+            Console.WriteLine(l1.lampadaNro);
+            Console.WriteLine(l1.comodo);
+            Console.WriteLine(l1.status);
+
+            Lampada l2 = new Lampada();
+
+            l2.provarExistencia();
+
+            l2.lampadaNro = 2;
+            l2.comodo = "sala de jantar";
+            l2.status = false;
+
+            Console.WriteLine(l2.lampadaNro);
+            Console.WriteLine(l2.comodo);
+            Console.WriteLine(l2.status);
+
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Usuario
+{
+    class Usuario
+    {
+        //atributos
+        public string nome;
+        public string email;
+        public string login;
+        public string senha;
+
+        //método específico da classe
+        public void provarExistencia()
+        {
+            Console.WriteLine("Oi, eu existo!");
+        }
+        //método principal
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Olá Mundo!");
+
+            //método construtor
+            //NomeDaClasse | nomeDaInstancia | = | comando new | método Construtor()
+            Usuario u1 = new Usuario();
+
+            //invocar o método provarExistencia()
+            //nomeDaInstacia.nomeDoMetodo();
+            u1.provarExistencia();
+
+            //inserindo dados no objeto (instância)
+            u1.nome = "Raíssa Victoria";
+            u1.email = "raissa@etec.com";
+            u1.login = "raissa";
+            u1.senha = "13579";
+
+            //exibindo os dados do objeto
+            Console.WriteLine(u1.nome);
+            Console.WriteLine(u1.email);
+            Console.WriteLine(u1.login);
+            Console.WriteLine(u1.senha);
+        }
+    }
+}
