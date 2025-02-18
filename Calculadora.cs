6dd55add1f506f89aa2e8ad700d8d2e9267cf56f
RaissaGois <168118466+RaissaGois@users.noreply.github.com>
2025-02-18 17:54:53 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        public double n1;
+        public double n2;
+
+        public void somar()
+        {
+            Console.WriteLine("A soma é: ");
+            Console.WriteLine(n1 + n2);
+
+        }
+        static void Main(string[] args)
+        {
+            Calculadora c1 = new Calculadora();
+
+            c1.n1 = 3;
+            c1.n2 = 2;
+            c1.somar();
+        }
+    }
+}
