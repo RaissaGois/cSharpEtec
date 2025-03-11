0e8e1a5117a7b13ed016c0319d540fb5e8ce39f3
RaissaGois <168118466+RaissaGois@users.noreply.github.com>
2025-03-11 17:27:42 -0300

    Add files via upload

---
+++ ContaCorrente/ContaCorrente/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ContaCorrente
+{
+    class ContaCorrente
+    {
+        public string nome;
+        public string cpf;
+        public double limiteCredito;
+        public double saldo;
+        public bool negativado;
+
+        public double depositar(double valor)
+        {
+            saldo = saldo + valor;
+            return saldo;
+        }
+        public double sacar(double valor)
+        {
+            saldo = saldo - valor;
+            return saldo;
+        }
+
+        public void verSaldo()
+        {
+            Console.WriteLine("Titular: " + nome);
+            Console.WriteLine("CPF: " + cpf);
+            Console.WriteLine("Saldo: R$" + saldo);
+        }
+    }
+}
+
---
+++ ContaCorrente/ContaCorrente/Principal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaCorrente
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente cc1 = new ContaCorrente();
+
+            cc1.nome = "Raissa Victoria";
+            cc1.cpf = "123.456.789-00";
+
+            cc1.depositar(1000);
+            cc1.sacar(500);
+            cc1.verSaldo();
+
+        }
+    }
+}
